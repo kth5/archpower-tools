fb2897f02497e36ae8f28312f9999ac41ffb2a43
Alexander Baldeck <alex.bldck@gmail.com>
2019-06-04 12:47:24 +0000

    * use base and fix default locale to en_US.UTF-8

--- docker/build.sh
+++ docker/build.sh
@@ -1,7 +1,19 @@
 #!/usr/bin/bash
 set -e
 
+PKG_REMOVE=(
+dhcpcd
+jfsutils
+linux-firmware
+linux
+man-pages
+systemd-resolvconf
+xfsprogs
+)
+
+
 rm -rf $(pwd)/sysroot
 mkdir -p $(pwd)/sysroot/var/lib/pacman
-pacman --noconfirm -r $(pwd)/sysroot -Sy pacman
-docker build --rm -t kth5/archpower:$(date "+%Y%m%d") -t kth5/archpower:latest .
+pacman --noconfirm -r $(pwd)/sysroot -Sy base 
+pacman --noconfirm -r $(pwd)/sysroot -Rcs ${PKG_REMOVE[@]}
+docker build --no-cache --rm -t kth5/archpower:$(date "+%Y%m%d") -t kth5/archpower:latest .
