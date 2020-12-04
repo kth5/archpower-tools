f4f93e5582e70eddd466e7c0e2ec82df903c666f
Alexander Baldeck <alex.bldck@gmail.com>
2020-12-04 14:29:00 +0100

    * sync in latest docker stuff

--- docker/build.sh
+++ docker/build.sh
@@ -2,6 +2,8 @@
 set -e
 
 rm -rf $(pwd)/sysroot
+mkdir -p $(pwd)/sysroot/etc/pacman.d
 mkdir -p $(pwd)/sysroot/var/lib/pacman
-pacman --noconfirm -r $(pwd)/sysroot -Sy pacman archlinux-keyring sed gzip
+touch $(pwd)/sysroot/etc/pacman.d/mirrorlist
+pacstrap -cM -C /etc/pacman.conf $(pwd)/sysroot base
 docker build --no-cache --rm -t kth5/archpower:$(date "+%Y%m%d") -t kth5/archpower:latest .
